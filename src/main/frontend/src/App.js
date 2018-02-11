cfc0ef8c715a301bf925828570b32c0a09b4e497
vgubarkov <vladimir.gubarkov@alice-app.com>
2018-02-11 22:37:37 +0200

    router

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Home from "./Home";
 import Users from "./Users";
 
 class App extends Component {
   render() {
-    return (
-      <Users/>
-    );
+      return (
+          <Router>
+              <div>
+                  <h2>Welcome to React Router Tutorial</h2>
+                  <ul>
+                      <li><Link to={'/'}>Home</Link></li>
+                      <li><Link to={'/users'}>Users</Link></li>
+                  </ul>
+                  <hr />
+
+                  <Switch>
+                      <Route exact path='/' component={Home} />
+                      <Route exact path='/users' component={Users} />
+                  </Switch>
+              </div>
+          </Router>
+      );
   }
 }
 

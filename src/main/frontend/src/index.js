a77db0dbe3dda54e31505494ad81f4075ebf4195
vgubarkov <vladimir.gubarkov@alice-app.com>
2018-02-11 17:15:50 +0200

    travail sur le react

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// console.info(process.env.REACT_APP_QQQ)
+// registerServiceWorker();
+unregister(); // TODO disabling for now till we understand wtf 

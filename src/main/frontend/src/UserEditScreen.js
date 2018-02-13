602c923c6faffd59ddd45a2e48c6575c0c85ea93
vgubarkov <vladimir.gubarkov@alice-app.com>
2018-02-13 20:37:46 +0200

    edit user screen

--- src/main/frontend/src/UserEditScreen.js
+++ src/main/frontend/src/UserEditScreen.js
@@ -6,7 +6,8 @@ export default class UserEditScreen extends Component {
         super(props);
         this.state = {
             userId: props.match.params.id
-        }
+        };
+        this.handleOnChange = this.handleOnChange.bind(this);
     }
 
     async componentDidMount() {
@@ -15,15 +16,32 @@ export default class UserEditScreen extends Component {
         this.setState({user: json})
     }
 
+    handleOnChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            user: {
+                ...this.state.user,
+                [name]: value
+            }
+        });
+    }
+
     render() {
         return this.state.user ? (
             <div>
                 <h3>Edit User</h3>
                 <div><b>Id: </b> {this.state.user.id}</div>
-                <div><b>Firstname: </b> {this.state.user.firstName}</div>
-                <div><b>Lastname: </b> {this.state.user.lastName}</div>
-                <div><b>Email: </b> {this.state.user.email}</div>
-                <div><b>Phone: </b> {this.state.user.phone}</div>
+                <div><b>Firstname: </b> 
+                    <input type="text" name={'firstName'} value={this.state.user.firstName} onChange={this.handleOnChange}/></div>
+                <div><b>Lastname: </b>
+                    <input type="text" name={'lastName'} value={this.state.user.lastName} onChange={this.handleOnChange}/></div>
+                <div><b>Email: </b>
+                    <input type="text" name={'email'} value={this.state.user.email} onChange={this.handleOnChange}/></div>
+                <div><b>Phone: </b>
+                    <input type="text" name={'phone'} value={this.state.user.phone} onChange={this.handleOnChange}/></div>
             </div>
         ) : (
             <div>Loading...</div>

58798190116dcaa98df01d33ae039f919bcd6a1e
vgubarkov <vladimir.gubarkov@alice-app.com>
2018-02-14 23:54:58 +0200

    work on react + security : work on ajax login

--- src/main/frontend/src/LoginScreen.js
+++ src/main/frontend/src/LoginScreen.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import {api} from './api';
 
 class LoginScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    handleOnChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    };
+    doLogin = async (event) => {
+        let res = await api.POST('login', {
+            login: this.state.login,
+            password: this.state.password
+        });
+        console.info(111,res)
+    };
     render() {
         return (
             <div>
                 <h2>Login</h2>
                 <div>
                     <div>
-                        Login: <input type={'text'} />
+                        Login: <input type={'text'} name={'login'} value={this.state.login}  onChange={this.handleOnChange}/>
                     </div>
                     <div>
-                        Password: <input type={'password'}/>
+                        Password: <input type={'password'} name={'password'} value={this.state.password}  onChange={this.handleOnChange}/>
                     </div>
-                    <button type={'button'}>Login</button>
+                    <button type={'button'} onClick={this.doLogin}>Login</button>
                 </div>
             </div>
         );
--- src/main/java/com/cmlteam/flywise/controllers/ApiController.java
+++ src/main/java/com/cmlteam/flywise/controllers/ApiController.java
@@ -1,14 +1,17 @@
 package com.cmlteam.flywise.controllers;
 
+import com.cmlteam.flywise.model.LoginRequest;
 import com.cmlteam.flywise.model.ResultStatus;
 import com.cmlteam.flywise.model.User;
 import com.cmlteam.flywise.services.AppSecurityService;
 import com.cmlteam.flywise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +23,43 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class ApiController {
     private final UserService userService;
     private final AppSecurityService appSecurityService;
+    //    @Qualifier("authenticationManager")
+    private  final AuthenticationManager authenticationManager;
 
     @Autowired
-    public ApiController(UserService userService, AppSecurityService appSecurityService) {
+    public ApiController(UserService userService, AppSecurityService appSecurityService, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.appSecurityService = appSecurityService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    /*@RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public LoginStatus getStatus() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && !auth.getName().equals("anonymousUser") && auth.isAuthenticated()) {
+            return new LoginStatus(true, auth.getName());
+        } else {
+            return new LoginStatus(false, null);
+        }
+    }*/
+
+    @RequestMapping(value = "login", method = POST)
+    public ResultStatus login(@RequestBody LoginRequest loginRequest) {
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                loginRequest.getLogin(), loginRequest.getPassword());
+
+//        User details = new User(username);
+//        token.setDetails(details);
+
+        try {
+            Authentication auth = authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            return ResultStatus.SUCCESS;
+        } catch (BadCredentialsException e) {
+            return ResultStatus.error(e.getMessage());
+        }
     }
 
     @RequestMapping(value = "user", method = GET)

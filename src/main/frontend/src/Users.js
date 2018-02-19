446c8a7a2c4764417418e55bcbc8297677e23a06
vgubarkov <vladimir.gubarkov@alice-app.com>
2018-02-19 22:51:58 +0200

    material design - user list

--- src/main/frontend/src/Users.js
+++ src/main/frontend/src/Users.js
@@ -1,30 +1,77 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import {connect} from 'react-redux';
 import {api} from './api';
-import User from "./User";
 import {usersLoadStarted, usersLoadSuccess} from "./redux/actions";
 import {Link} from "react-router-dom";
+import {withStyles} from "material-ui/styles/index";
+import Table, {TableBody, TableCell, TableHead, TableRow} from 'material-ui/Table';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
 
 class Users extends Component {
+    static propTypes = {
+        classes: PropTypes.object.isRequired,
+    };
+
     async componentDidMount() {
         this.props.dispatch(usersLoadStarted());
         const json = await api.GET('user');
         this.props.dispatch(usersLoadSuccess(json));
     }
-    
+
+    canEdit = (u) => (
+        u.id === this.props.currentUser.id || this.props.currentUser.role === 'ROLE_ADMIN'
+    );
+
     render() {
+        const { classes } = this.props;
+
         return (
             <div className="users">
                 <h2>Users</h2>
-                {this.props.users.map(u =>
-                    <User key={u.id}
-                          currentUser={this.props.currentUser}
-                          {...u}
-                    />
-                )}
                 {
                     this.props.currentUser.role === 'ROLE_ADMIN' && <Link to={`/user/add`}>Add</Link>
                 }
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Username</TableCell>
+                            <TableCell>Full name</TableCell>
+                            <TableCell>Role</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.users.map(u => {
+                            return (
+                                <TableRow key={u.id}>
+                                    <TableCell>{u.id}</TableCell>
+                                    <TableCell>{u.username}</TableCell>
+                                    <TableCell>{u.firstName} {u.lastName}</TableCell>
+                                    <TableCell>{u.role}</TableCell>
+                                    <TableCell>
+                                        <Link to={`/user/${u.id}`}>View</Link>
+                                        {' '}
+                                        {
+                                            this.canEdit(u) && <Link to={`/user/${u.id}/edit`}>Edit</Link>
+                                        }
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
             </div>
         );
     }
@@ -37,4 +84,4 @@ const mapStateToProps = state => {
     }
 };
 
-export default Users = connect(mapStateToProps)(Users);
+export default Users = withStyles(styles)(connect(mapStateToProps)(Users));
